2ee4f5d8f1f49a9288a28015a7004c9c5ed6c80a
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-12-05 18:22:38 +0530

    Start create front page UI

---
+++ login_system/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:login_system/ui/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Login SignUp System',
+    home: new Home(),
+  ));
+}
