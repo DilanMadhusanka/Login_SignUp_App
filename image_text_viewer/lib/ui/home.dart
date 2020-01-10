4959de3333d471116fbf5880fd1b8f7c89648c81
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-01-10 22:40:28 +0530

    remove the previous package and create new package and set background

---
+++ image_text_viewer/lib/ui/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent.shade700,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/background.jpg"),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Column(
+          
+        ),
+      ),
+    );
+  }
+}
